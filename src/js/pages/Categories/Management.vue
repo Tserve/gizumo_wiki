ee82e537163f96d6271c75622499a39ec4d4eb30
n-komatsu <komatsu@gizumo-inc.jp>
2019-07-23 15:22:47 +0900

    #17 カテゴリー関連のpagesコンポーネントのロジック部分を全削除

--- src/js/pages/Categories/Management.vue
+++ src/js/pages/Categories/Management.vue
@@ -1,26 +1,10 @@
 <template lang="html">
   <div class="category-management">
     <section class="category-management-post">
-      <app-category-post
-        :category="category"
-        :disabled="loading ? true : false"
-        :error-message="errorMessage"
-        :done-message="doneMessage"
-        :access="access"
-        @udpateValue="updateValue"
-        @clearMessage="clearMessage"
-        @handleSubmit="handleSubmit"
-      />
+      <app-category-post />
     </section>
     <section class="category-management-list">
-      <app-category-list
-        :theads="theads"
-        :categories="categoryList"
-        :delete-category-name="deleteCategoryName"
-        :access="access"
-        @openModal="openModal"
-        @handleClick="deleteCategory"
-      />
+      <app-category-list />
     </section>
   </div>
 </template>
@@ -35,68 +19,6 @@ export default {
     appCategoryList: CategoryList,
   },
   mixins: [Mixins],
-  data() {
-    return {
-      category: '',
-      theads: ['カテゴリー名', '', '', ''],
-    };
-  },
-  computed: {
-    access() {
-      return this.$store.getters['auth/access'];
-    },
-    loading() {
-      return this.$store.state.categories.loading;
-    },
-    errorMessage() {
-      return this.$store.state.categories.errorMessage;
-    },
-    doneMessage() {
-      return this.$store.state.categories.doneMessage;
-    },
-    categoryList() {
-      return this.$store.state.categories.categoryList;
-    },
-    deleteCategoryId() {
-      return this.$store.state.categories.deleteCategoryId;
-    },
-    deleteCategoryName() {
-      return this.$store.state.categories.deleteCategoryName;
-    },
-  },
-  created() {
-    this.$store.dispatch('categories/clearMessage');
-    this.$store.dispatch('categories/getAllCategories');
-  },
-  methods: {
-    updateValue($event) {
-      this[$event.target.name] = $event.target.value;
-    },
-    clearMessage() {
-      this.$store.dispatch('categories/clearMessage');
-    },
-    handleSubmit() {
-      if (this.loading) return;
-      this.$store.dispatch('categories/postCateogry', this.category)
-        .then(() => {
-          this.category = '';
-          this.$store.dispatch('categories/getAllCategories');
-        });
-    },
-    openModal(categoryId, categoryName) {
-      this.toggleModal();
-      this.$store.dispatch('categories/clearMessage');
-      this.$store.dispatch('categories/confirmDeleteCategory',
-        { categoryId, categoryName });
-    },
-    deleteCategory() {
-      this.$store.dispatch('categories/deleteCategory', this.deleteCategoryId)
-        .then(() => {
-          this.$store.dispatch('categories/getAllCategories');
-        });
-      this.toggleModal();
-    },
-  },
 };
 </script>
 
